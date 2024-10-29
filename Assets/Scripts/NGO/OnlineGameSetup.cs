5253259205667b9a57ac886b98a19f7502481a92
YurkevichVA <bekker.volodymyr.yu@gmail.com>
2024-10-29 12:58:13 +0200

    word generation but it`s not sync

--- Assets/Scripts/NGO/OnlineGameSetup.cs
+++ Assets/Scripts/NGO/OnlineGameSetup.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class OnlineGameSetup : MonoBehaviour
@@ -5,8 +6,15 @@ public class OnlineGameSetup : MonoBehaviour
     [SerializeField] private StartTurnPanelUI _startTurnPanel;
     [SerializeField] private TurnHandler _turnHandler;
 
+    [SerializeField] private WordsPanel _wordsPanel;
+    [SerializeField] private WordManager _wordManager;
+    private List<string> _words = new List<string>();
+
     private void Start()
     {
         _turnHandler.OnTurnChanged += _startTurnPanel.OnTurnChanged;
+
+        _words = _wordManager.FormWordListForRound();
+        _wordsPanel.InitPanel(_words);
     }
 }
