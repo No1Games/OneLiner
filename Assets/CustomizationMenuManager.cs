e7eaf8b6bb9eed15e57b20149aa09c964101bf0f
Nikolskyi-Ihor <nikolskiylitod@gmail.com>
2024-11-12 20:34:55 +0200

    VisualUpdate 4.2

---
+++ Assets/CustomizationMenuManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizationMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject avatarShopPanel;
+    [SerializeField] GameObject avatarBackShopPanel;
+    [SerializeField] GameObject nameBackShopPanel;
+    [SerializeField] GameObject avatarShopActiveBtn;
+    [SerializeField] GameObject nameBackShopActiveBtn;
+    [SerializeField] GameObject avatarBackShopActiveBtn;
+    
+    // Start is called before the first frame update
+    
+}
---
+++ Assets/Scripts/MainMenuCode/Tab.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Tab
+{
+    public GameObject activeImage;
+    public GameObject inactiveImage;
+    public GameObject contentPanel;
+}
