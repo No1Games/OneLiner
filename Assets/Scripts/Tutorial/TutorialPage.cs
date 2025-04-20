4dfb9bf3d2009b15e265b5a9b5e6c9c1a61fe7f7
bekkerVU <bekker.volodymyr.yu@gmail.com>
2025-04-20 12:33:15 +0300

    Revert "TutorialLocalization"
    This reverts commit 4b34fcf09814026dc04882d8a93cfce93f37f46a.

--- Assets/Scripts/Tutorial/TutorialPage.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-
-public class TutorialPage : MonoBehaviour
-{
-    [SerializeField] private Button tutorialButton;
-    public Button TutorialButton => tutorialButton;
-}
