1798e94c1c1d77c5739e595e6920968134813cfa
Nikolskyi-Ihor <nikolskiylitod@gmail.com>
2025-04-22 14:06:16 +0300

    Reapply "TutorialLocalization"
    This reverts commit 4dfb9bf3d2009b15e265b5a9b5e6c9c1a61fe7f7.

---
+++ Assets/Scripts/Tutorial/TutorialPage.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPage : MonoBehaviour
+{
+    [SerializeField] private Button tutorialButton;
+    public Button TutorialButton => tutorialButton;
+}
